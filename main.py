86a00654571316c3268fd4578a8e80ef2c16dbbd
IOConcept <123223096+IOConcept@users.noreply.github.com>
2023-03-11 05:02:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import json
 
 
+def validate_input(prompt, validator):
+    while True:
+        try:
+            value = validator(input(prompt))
+            return value
+        except ValueError:
+            print("Invalid input, please try again.")
+
+
+def validate_int(value):
+    return int(value)
+
+
+def validate_choice(value):
+    value = int(value)
+    if value not in range(1, 5):
+        raise ValueError
+    return str(value)
+
+
 def load_recipes():
     try:
         with open("recipes.json", "r") as f:
@@ -16,27 +36,47 @@ def load_recipes():
 
 
 def save_recipes(recipes):
-    with open("recipes.json", "w") as f:
-        json.dump(recipes, f, indent=4)
+    try:
+        with open("recipes.json", "w") as f:
+            json.dump(recipes, f, indent=4)
+    except IOError:
+        print("Failed to save recipes to file.")
+
+    try:
+        with open("recipes.txt", "w") as f:
+            for index, recipe in enumerate(recipes["recipes"]):
+                f.write(f"{index + 1}. {recipe['name']}\n")
+                f.write("Ingredients:\n")
+                for ingredient in recipe["ingredients"]:
+                    f.write(f"- {ingredient}\n")
+                f.write("Steps:\n")
+                for step in recipe["steps"]:
+                    f.write(f"- {step}\n")
+                f.write("\n")
+    except IOError:
+        print("Failed to save recipes to file.")
 
 
 def display_recipe(recipe):
-    print("Name:", recipe.get("name"))
-    print("Ingredients:")
-    for ingredient in recipe.get("ingredients"):
-        print("-", ingredient)
-    print("Steps:")
-    for step in recipe.get("steps"):
-        print("-", step)
+    if recipe:
+        print("Name:", recipe.get("name"))
+        print("Ingredients:")
+        for ingredient in recipe.get("ingredients"):
+            print(f"- {ingredient}")
+        print("Steps:")
+        for step in recipe.get("steps"):
+            print(f"- {step}")
+    else:
+        print("Recipe not found.")
 
 
 def view_recipe():
-    name = input("Enter the name of the recipe: ")
+    name = input("Recipe: ")
     if not name:
         return display_all_recipes()
     recipes = load_recipes()
-    for recipe in recipes["recipes"]:
-        if recipe.get("name") == name:
+    for index, recipe in enumerate(recipes["recipes"]):
+        if recipe.get("name") == name or str(index+1) == name:
             display_recipe(recipe)
             return
     print("That's not in my belly")
@@ -57,9 +97,11 @@ def add_recipe():
             break
         steps.append(step)
     recipes = load_recipes()
-    recipes["recipes"].append({"name": name, "ingredients": ingredients, "steps": steps})
+    recipe = {"name": name, "ingredients": ingredients, "steps": steps}
+    recipes["recipes"].append(recipe)
+    recipe_index = len(recipes["recipes"])
     save_recipes(recipes)
-    print("1 recipe added to my belly")
+    print(f"{recipe_index}. recipe added to my belly")
 
 
 def display_all_recipes():
@@ -67,53 +109,31 @@ def display_all_recipes():
     if not recipes["recipes"]:
         print("No recipes in my belly")
         return
-    for recipe in recipes["recipes"]:
-        print(recipe.get("name"))
-
-
-def export_recipes():
-    recipes = load_recipes()
-    if not recipes["recipes"]:
-        print("No recipes in my belly")
-        return
-    with open("recipes.txt", "w") as f:
-        for recipe in recipes["recipes"]:
-            f.write(f"Name: {recipe.get('name')}\n")
-            f.write("Ingredients:\n")
-            for ingredient in recipe.get("ingredients"):
-                f.write(f"- {ingredient}\n")
-            f.write("Steps:\n")
-            for step in recipe.get("steps"):
-                f.write(f"- {step}\n")
-            f.write("\n")
-    print("Recipes exported successfully")
-
-
+    for index, recipe in enumerate(recipes["recipes"]):
+        print(f"{index + 1}. {recipe.get('name')}")
 def main():
+    version = "Pre-Release 4"
+    print(f"# Version {version}")
     while True:
-        print("Menu:")
-        print("1. View Recipe")
+        print()
+        print("What would you like to do?")
+        print("1. View a recipe")
         print("2. Add a recipe")
-        print("3. Export Recipes")
-        print("4. Display all recipes")
-        print("5. Quit")
-
-        choice = input("Enter your choice: ")
-
+        print("3. Display all recipes")
+        print("4. Exit")
+        choice = validate_input("Enter your choice (1-4): ", validate_choice)
         if choice == "1":
             view_recipe()
         elif choice == "2":
             add_recipe()
         elif choice == "3":
-            export_recipes()
-        elif choice == "4":
             display_all_recipes()
-        elif choice == "5":
-            break
+        elif choice == "4":
+            print("Goodbye!")
+            return
         else:
-            print("Invalid choice. Please try again.")
+            print("That's not a valid choice, please try again")
 
 
 if __name__ == "__main__":
-    print("# Version 20.3 Beta 3")
     main()
